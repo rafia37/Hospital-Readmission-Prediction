bbb61772847df6b4b8512ce3fc07611edda14446
Rafia Bushra <rafia0037@gmail.com>
2020-11-22 11:03:04 -0600

    performed model evaluations

--- PredictionModel.R
+++ PredictionModel.R
@@ -15,43 +15,31 @@ library(caTools) # To split data
 library(Metrics) # To calculate RMSE value
 
 #Reading in train and test data
-train <- read.csv("hm7-Train.csv", na.strings = c("","NA","<NA>"))
-test <- read.csv("hm7-Test.csv", na.strings = c("","NA","<NA>"))
-
-
-#Choosing only predictor columns and target
-new_train <- select(train, -c(race, diagnosis, payer_code, medical_specialty, max_glu_serum:metformin.pioglitazone))
-new_test <- select(test, -c(race, diagnosis, payer_code, medical_specialty, max_glu_serum:metformin.pioglitazone))
-new_train$readmitted <- as.factor(new_train$readmitted)
-#new_train <- drop_na(new_train)
+train <- read.csv("../cleaned_train_data.csv", na.strings = c("","NA","<NA>"))
+test <- read.csv("../cleaned_test_data.csv", na.strings = c("","NA","<NA>"))
 
 
 
 #Logistic Regression
 #--------------------
-fit <- glm(data=new_train, readmitted ~ .-patientID, family="binomial")
+fit <- glm(data=train, readmitted ~ .-patientID, family="binomial")
 
 logLoss(fit)
 
 
 #Evaluating model
-new_train$pred<-as.numeric(fit$fitted.values>0.5)
-#names(new_train)[names(new_train)=="readmitted"] <- "targ"
-#new_train$pred <- as.factor(new_train$pred)
-#new_train$targ <- as.factor(new_train$targ)
-model_eval(fit, new_train, "readmitted")
+train$pred<-as.numeric(fit$fitted.values>0.5)
+model_eval(fit, train)
 
 
 #Making predictions on test data
-pred_prob <- predict(fit, new_test, type = "response")
-new_test$predReadmit <- pred_prob
+pred_prob <- predict(fit, test, type = "response")
+test$predReadmit <- pred_prob
 
 
 #model_eval()
-submission <- select(new_test, c(patientID, predReadmit))
+submission <- select(test, c(patientID, predReadmit))
 write.csv(submission, "../hm7-group11-submission.csv", row.names = FALSE)
-
-
 
 
 
@@ -65,10 +53,10 @@ for (i in 1:10) {
                           maxdepth = i,
                           cp = 0)
   
-  fit_dt <- rpart(readmitted~.-patientID, data = new_train, method = 'class', control = tuning)
+  fit_dt <- rpart(readmitted~.-patientID, data = train, method = 'class', control = tuning)
   
   a <- as.data.frame(predict(fit_dt, type="prob")) 
-  lloss <- logLoss(new_train$readmitted, as.numeric(unlist(a["1"])))
+  lloss <- logLoss(train$readmitted, as.numeric(unlist(a["1"])))
   
   split <- c(split, i)
   ll <- c(ll, lloss)
@@ -76,12 +64,12 @@ for (i in 1:10) {
   print(lloss)
 }
 
-pred_prob_dt <-predict(fit_dt, new_test, type = 'prob')
-new_test$predReadmit <- pred_prob_dt
+pred_prob_dt <-predict(fit_dt, test, type = 'prob')
+test$predReadmit <- pred_prob_dt
 
 
 #model_eval()
-submission_dt <- select(new_test, c(patientID, predReadmit))
+submission_dt <- select(test, c(patientID, predReadmit))
 write.csv(submission_dt, "../hm7-group11-submission_dt4.csv", row.names = FALSE)
 
 
@@ -98,11 +86,11 @@ caret.control <- trainControl(method = "repeatedcv",
                               number = 5,
                               repeats = 3)
 
-new_train$readmitted <- as.factor(new_train$readmitted)
+train$readmitted <- as.factor(train$readmitted)
 
 #using 15 values for tuning the cp parameter for rpart. 
 fit_dt <- train(readmitted ~ .-patientID, 
-                data = new_train,
+                data = train,
                 method = "rpart",
                 trControl = caret.control,
                 tuneLength = 15)
@@ -114,12 +102,12 @@ bestDT
 
 prp(bestDT, type = 0, extra = 1, under = TRUE)
 
-pred_prob_dt <- predict(fit_dt, new_test, type="prob")
-new_test$predReadmit <- pred_prob_dt
+pred_prob_dt <- predict(fit_dt, test, type="prob")
+test$predReadmit <- pred_prob_dt
 
 
 #model_eval()
-submission_dt <- select(new_test, c(patientID, predReadmit))
+submission_dt <- select(test, c(patientID, predReadmit))
 write.csv(submission_dt, "../hm7-group11-submission_dt1.csv", row.names = FALSE)
 
 
@@ -137,19 +125,19 @@ set.seed(5103)
 #train model
 
 for (i in 10:30) {
-  fit_rf <- randomForest(readmitted ~ .-patientID, data = new_train, mtry=2, ntree = 50, 
+  fit_rf <- randomForest(readmitted ~ .-patientID, data = train, mtry=2, ntree = 50, 
                          maxnodes=i)
   
   ll <- logLoss(fit_rf)
   print(ll)
 }
 
-pred_prob_rf <- predict(fit_rf, new_test, type="prob")
-new_test$predReadmit <- pred_prob_rf
+pred_prob_rf <- predict(fit_rf, test, type="prob")
+test$predReadmit <- pred_prob_rf
 
 
 #model_eval()
-submission_dt <- select(new_test, c(patientID, predReadmit))
+submission_dt <- select(test, c(patientID, predReadmit))
 write.csv(submission_dt, "../hm7-group11-submission_rf.csv", row.names = FALSE)
 
 
@@ -158,32 +146,35 @@ write.csv(submission_dt, "../hm7-group11-submission_rf.csv", row.names = FALSE)
 #Support Vector Machine
 #------------------------
 
-fit_svm <- svm(readmitted ~ .-patientID, data = new_train, type="C-classification",
-               kernel="polynomial", probability=TRUE)
+fit_svm <- svm(readmitted ~ .-patientID, data = train, type="C-classification",
+               kernel="linear", probability=TRUE)
 
 a <- predict(fit_svm, probability = TRUE)
 
-pred_prob_svm <- predict(fit_svm, new_test, probability = TRUE)
-new_test$predReadmit <- attr(pred_prob_svm, "probabilities")
+pred_prob_svm <- predict(fit_svm, test, probability = TRUE)
+test$predReadmit <- attr(pred_prob_svm, "probabilities")
 
 
 #model_eval()
-submission_svm <- select(new_test, c(patientID, predReadmit))
+submission_svm <- select(test, c(patientID, predReadmit))
 write.csv(submission_svm, "../hm7-group11-submission_svm_poly.csv", row.names = FALSE)
+
+
+
 
 # Neural Networks
 #---------------------
-glimpse(new_train)
-hosp_matrix = model.matrix(~., data =new_train)
-test_matrix = model.matrix(~.,data=new_test)
+glimpse(train)
+hosp_matrix = model.matrix(~., data =train)
+test_matrix = model.matrix(~.,data=test)
 
-hosp = new_train %>%
+hosp = train %>%
   select(-c('time_in_hospital', 'age','num_medications','number_outpatient','number_emergency', 'num_procedures','gender','diabetesMed'))
 
 str(hosp)
 
 hosp_matrix <- model.matrix(~.,data=hosp)
-test <- new_test %>% 
+test <- test %>% 
   select(-c('predReadmit','time_in_hospital', 'age','num_medications','number_outpatient','number_emergency', 'num_procedures','gender','diabetesMed'))
 empty <- test$patientID*0
 test <- cbind(test,empty)
@@ -223,7 +214,7 @@ nn1$result.matrix
 
 nn1$generalized.weights # To check the weights
 test = hosp_matrix_[,-c(1,22)]
-str(new_train)
+str(train)
 
 pred = neuralnet::compute(nn1, test_matrix[,-c(1,22)]) # To predict on test set
 class_pred = round(pred$net.result)
